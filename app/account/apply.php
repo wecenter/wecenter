cdffa3c72ed5e0239678d11f68abce2e90b0fa6b
cdl <cdl@2065f616-0316-476b-a4df-6472c8b1327c>
2012-12-12 09:27:01 +0000

    bugfix

--- app/account/apply.php
+++ app/account/apply.php
@@ -23,7 +23,7 @@ class apply extends AWS_CONTROLLER
 
 	public function get_access_rule()
 	{
-		$rule_action['rule_type'] = 'black'; //黑名单,黑名单中的检查  'white'白名单,白名单以外的检查
+		$rule_action['rule_type'] = 'black';
 		$rule_action['actions'] = array();
 		
 		return $rule_action;
