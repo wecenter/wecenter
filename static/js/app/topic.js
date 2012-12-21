345ce3367df1cf7f766807283d7c1a0b4198c2e1
cdl <cdl@2065f616-0316-476b-a4df-6472c8b1327c>
2012-12-21 07:40:23 +0000

    新触屏版

--- static/js/app/topic.js
+++ static/js/app/topic.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 		
 		bp_more_load(G_BASE_URL + '/topic/ajax/question_list/type-best__topic_id-' + CONTENTS_TOPIC_ID, $('#bp_best_question_more'), $('#c_best_question_list'));
 		
-		bp_more_load(G_BASE_URL + '/question/ajax/discuss/answer_count-0__topic_id-' + CONTENTS_TOPIC_ID, $('#bp_noanswer_more'), $('#c_noanswer_list'), 1);
+		bp_more_load(G_BASE_URL + '/question/ajax/discuss/topic_id-' + CONTENTS_TOPIC_ID, $('#c_question_more'), $('#c_question_list'), 1);
 		
 		bp_more_load(G_BASE_URL + '/topic/ajax/question_list/type-favorite__topic_title-' + encodeURIComponent(CONTENTS_TOPIC_TITLE), $('#bp_favorite_more'), $('#c_favorite_list'), 0, function () { if ($('#c_favorite_list a').attr('id')) { $('#i_favorite').show() } });
 	}
