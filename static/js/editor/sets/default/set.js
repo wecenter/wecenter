8957095cadf3f66dd205af8b60d745b78b7982dd
cloeft <cloeft@2065f616-0316-476b-a4df-6472c8b1327c>
2013-01-22 15:43:40 +0000

    修复codemirror模式下点击按钮插入文本后光标定位错误 修复list模式回车，保持上一行模式

--- static/js/editor/sets/default/set.js
+++ static/js/editor/sets/default/set.js
@@ -17,22 +17,22 @@ myMarkdownSettings = {
         $('.i_replay_but2').click();
     }},
     markupSet: [
-        {name:_t('粗体'), key:"B", openWith:'**', closeWith:'**'},
-        {name:_t('斜体'), key : "I", openWith : '*', closeWith : '*'},
+        {name:_t('粗体'), key:"B", openWith:'**', closeWith:'**', offset:2},
+        {name:_t('斜体'), key : "I", openWith : '*', closeWith : '*', offset:1},
         {separator:'---------------' },        
-        {name:_t('引用'), openWith:'> '},
-        {name:_t('代码'), openWith:'{{{\n', closeWith:'\n}}}'},
+        {name:_t('引用'), openWith:'> ', offset:2},
+        {name:_t('代码'), openWith:'{{{\n', closeWith:'\n}}}', addline:1},
         {separator:'---------------' },
-        {name:_t('普通列表'), openWith:'- ' },
+        {name:_t('普通列表'), openWith:'- ', offset:2 },
         {name:_t('数字列表'), openWith:function(markItUp) {
             return markItUp.line+'. ';
-        }},
+        }, offset:3},
         {separator:'---------------' },
         {name:_t('图片'), key:"P", openWith:function(){$.uploadPicture()}},
         {name:_t('视频'), key:"Y", openWith:function(){$.uploadVideo()}},
         {separator:'---------------'},
-        {name:_t('大标题'), key : "1", openWith:'\n## '},
-        {name:_t('小标题'), key : "2", openWith : '\n### ' },
+        {name:_t('大标题'), key : "1", openWith:'\n## ', offset:3, addline:1},
+        {name:_t('小标题'), key : "2", openWith : '\n### ', offset:4, addline:1},
         {separator:'---------------'},
         {name : _t('预览模式'), openWith:function(){
             $('.markItUpButton11 a').toggleClass('cur');
