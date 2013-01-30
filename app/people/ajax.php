99e4d4e2f1bbbfa205392a9190d6d5129b2e9222
cdl <cdl@2065f616-0316-476b-a4df-6472c8b1327c>
2013-01-30 09:09:41 +0000

    Bug fix

--- app/people/ajax.php
+++ app/people/ajax.php
@@ -1,182 +1,1 @@
-<?php
-/*
-+--------------------------------------------------------------------------
-|   Anwsion [#RELEASE_VERSION#]
-|   ========================================
-|   by Anwsion dev team
-|   (c) 2011 - 2012 Anwsion Software
-|   http://www.anwsion.com
-|   ========================================
-|   Support: zhengqiang@gmail.com
-|   
-+---------------------------------------------------------------------------
-*/
-
-define('IN_AJAX', TRUE);
-
-
-if (!defined('IN_ANWSION'))
-{
-	die;
-}
-
-class ajax extends AWS_CONTROLLER
-{
-
-	public function get_access_rule()
-	{
-		$rule_action['rule_type'] = 'white'; //黑名单,黑名单中的检查  'white'白名单,白名单以外的检查
-		
-		$rule_action['actions'] = array(
-			'user_info'
-		);
-		
-		if ($this->user_info['permission']['visit_people'])
-		{
-			$rule_action['actions'][] = 'user_actions';
-			$rule_action['actions'][] = 'follows';
-			$rule_action['actions'][] = 'topics';
-		}
-		
-		return $rule_action;
-	}
-
-	public function setup()
-	{
-		$this->per_page = get_setting('contents_per_page');
-		
-		HTTP::no_cache_header();
-	}
-
-	public function user_actions_action()
-	{
-		if ((isset($_GET['perpage']) && intval($_GET['perpage']) > 0))
-		{
-			$this->per_page = intval($_GET['perpage']);
-		}
-		
-		$data = $this->model('account')->get_user_actions($_GET['uid'], (intval($_GET['page']) * $this->per_page) . ", {$this->per_page}", $_GET['actions'], $this->user_id, !$_GET['distint']);
-		
-		TPL::assign('list', $data);
-		
-		if ($_GET['template'] == 'm')
-		{
-			$template_dir = 'm';
-		}
-		else
-		{
-			$template_dir = 'people';
-		}
-		
-		if ($_GET['actions'] == '201')
-		{
-			TPL::output($template_dir . '/ajax/user_actions_questions_201');
-		}
-		else if ($_GET['actions'] == '101')
-		{
-			TPL::output($template_dir . '/ajax/user_actions_questions_101');
-		}
-		else
-		{
-			TPL::output($template_dir . '/ajax/user_actions');
-		}
-	}
-	
-	public function user_info_action()
-	{
-		if ($this->user_id == $_GET['uid'])
-		{
-			$user_info = $this->user_info;
-		}
-		else if (!$user_info = $this->model('account')->get_user_info_by_uid($_GET['uid'], ture))
-		{
-			H::ajax_json_output(array(
-				'uid' => null
-			));
-		}
-		
-		if ($this->user_id != $user_info['uid'])
-		{
-			$user_follow_check = $this->model('follow')->user_follow_check($this->user_id, $user_info['uid']);
-		}
-		
-		H::ajax_json_output(array(
-			'reputation' => $user_info['reputation'],
-			'agree_count' => $user_info['agree_count'],
-			'thanks_count' => $user_info['thanks_count'],
-			'type' => 'people',
-			'uid' => $user_info['uid'],
-			'user_name' => $user_info['user_name'],
-			'avatar_file' => get_avatar_url($user_info['uid'], 'mid'),
-			'signature' => $user_info['signature'],
-			'focus' => ($user_follow_check ? true : false),
-			'is_me' => (($this->user_id == $user_info['uid']) ? true : false),
-			'url' => get_js_url('/people/' . $user_info['url_token']),
-			'category_enable' => ((get_setting('category_enable') == 'Y') ? 1 : 0),
-			'verified' => $user_info['verified']
-		));
-	}
-	
-	public function follows_action()
-	{
-		switch ($_GET['type'])
-		{
-			case 'follows':
-				$users_list = $this->model('follow')->get_user_friends($_GET['uid'], (intval($_GET['page']) * $this->per_page) . ", {$this->per_page}");
-			break;
-			
-			case 'fans':
-				$users_list = $this->model('follow')->get_user_fans($_GET['uid'], (intval($_GET['page']) * $this->per_page) . ", {$this->per_page}");
-			break;
-		}
-		
-		if ($users_list AND $this->user_id)
-		{
-			foreach ($users_list as $key => $val)
-			{
-				$users_ids[] = $val['uid'];
-			}
-			
-			if ($users_ids)
-			{
-				$follow_checks = $this->model('follow')->users_follow_check($this->user_id, $users_ids);
-					
-				foreach ($users_list as $key => $val)
-				{
-					$users_list[$key]['follow_check'] = $follow_checks[$val['uid']];
-				}
-			}
-		}
-		
-		TPL::assign('users_list', $users_list);
-		
-		TPL::output('people/ajax/follows');
-	}
-
-	public function topics_action()
-	{
-		if ($topic_list = $this->model('topic')->get_focus_topic_list($_GET['uid'], (intval($_GET['page']) * $this->per_page) . ", {$this->per_page}") AND $this->user_id)
-		{
-			$topic_ids = array();
-			
-			foreach ($topic_list as $key => $val)
-			{
-				$topic_ids[] = $val['topic_id'];
-			}
-			
-			if ($topic_ids)
-			{
-				$topic_focus = $this->model('topic')->has_focus_topics($this->user_id, $topic_ids);
-				
-				foreach ($topic_list as $key => $val)
-				{
-					$topic_list[$key]['has_focus'] = $topic_focus[$val['topic_id']];
-				}
-			}
-		}
-		
-		TPL::assign('topic_list', $topic_list);
-		
-		TPL::output('people/ajax/topics');
-	}
-}
+<?php/*+--------------------------------------------------------------------------|   Anwsion [#RELEASE_VERSION#]|   ========================================|   by Anwsion dev team|   (c) 2011 - 2012 Anwsion Software|   http://www.anwsion.com|   ========================================|   Support: zhengqiang@gmail.com|   +---------------------------------------------------------------------------*/define('IN_AJAX', TRUE);if (!defined('IN_ANWSION')){	die;}class ajax extends AWS_CONTROLLER{	public function get_access_rule()	{		$rule_action['rule_type'] = 'white'; //黑名单,黑名单中的检查  'white'白名单,白名单以外的检查				$rule_action['actions'] = array(			'user_info'		);				if ($this->user_info['permission']['visit_people'])		{			$rule_action['actions'][] = 'user_actions';			$rule_action['actions'][] = 'follows';			$rule_action['actions'][] = 'topics';		}				return $rule_action;	}	public function setup()	{		$this->per_page = get_setting('contents_per_page');				HTTP::no_cache_header();	}	public function user_actions_action()	{		if ((isset($_GET['perpage']) && intval($_GET['perpage']) > 0))		{			$this->per_page = intval($_GET['perpage']);		}				$data = $this->model('account')->get_user_actions($_GET['uid'], (intval($_GET['page']) * $this->per_page) . ", {$this->per_page}", $_GET['actions'], $this->user_id);				TPL::assign('list', $data);				if ($_GET['template'] == 'm')		{			$template_dir = 'm';		}		else		{			$template_dir = 'people';		}				if ($_GET['actions'] == '201')		{			TPL::output($template_dir . '/ajax/user_actions_questions_201');		}		else if ($_GET['actions'] == '101')		{			TPL::output($template_dir . '/ajax/user_actions_questions_101');		}		else		{			TPL::output($template_dir . '/ajax/user_actions');		}	}		public function user_info_action()	{		if ($this->user_id == $_GET['uid'])		{			$user_info = $this->user_info;		}		else if (!$user_info = $this->model('account')->get_user_info_by_uid($_GET['uid'], ture))		{			H::ajax_json_output(array(				'uid' => null			));		}				if ($this->user_id != $user_info['uid'])		{			$user_follow_check = $this->model('follow')->user_follow_check($this->user_id, $user_info['uid']);		}				H::ajax_json_output(array(			'reputation' => $user_info['reputation'],			'agree_count' => $user_info['agree_count'],			'thanks_count' => $user_info['thanks_count'],			'type' => 'people',			'uid' => $user_info['uid'],			'user_name' => $user_info['user_name'],			'avatar_file' => get_avatar_url($user_info['uid'], 'mid'),			'signature' => $user_info['signature'],			'focus' => ($user_follow_check ? true : false),			'is_me' => (($this->user_id == $user_info['uid']) ? true : false),			'url' => get_js_url('/people/' . $user_info['url_token']),			'category_enable' => ((get_setting('category_enable') == 'Y') ? 1 : 0),			'verified' => $user_info['verified']		));	}		public function follows_action()	{		switch ($_GET['type'])		{			case 'follows':				$users_list = $this->model('follow')->get_user_friends($_GET['uid'], (intval($_GET['page']) * $this->per_page) . ", {$this->per_page}");			break;						case 'fans':				$users_list = $this->model('follow')->get_user_fans($_GET['uid'], (intval($_GET['page']) * $this->per_page) . ", {$this->per_page}");			break;		}				if ($users_list AND $this->user_id)		{			foreach ($users_list as $key => $val)			{				$users_ids[] = $val['uid'];			}						if ($users_ids)			{				$follow_checks = $this->model('follow')->users_follow_check($this->user_id, $users_ids);									foreach ($users_list as $key => $val)				{					$users_list[$key]['follow_check'] = $follow_checks[$val['uid']];				}			}		}				TPL::assign('users_list', $users_list);				TPL::output('people/ajax/follows');	}	public function topics_action()	{		if ($topic_list = $this->model('topic')->get_focus_topic_list($_GET['uid'], (intval($_GET['page']) * $this->per_page) . ", {$this->per_page}") AND $this->user_id)		{			$topic_ids = array();						foreach ($topic_list as $key => $val)			{				$topic_ids[] = $val['topic_id'];			}						if ($topic_ids)			{				$topic_focus = $this->model('topic')->has_focus_topics($this->user_id, $topic_ids);								foreach ($topic_list as $key => $val)				{					$topic_list[$key]['has_focus'] = $topic_focus[$val['topic_id']];				}			}		}				TPL::assign('topic_list', $topic_list);				TPL::output('people/ajax/topics');	}}
--- static/js/app/people.js
+++ static/js/app/people.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 			  
 	bp_more_load(G_BASE_URL + '/people/ajax/user_actions/uid-' + PEOPLE_USER_ID + '__actions-101', $('#bp_user_actions_questions_more'), $('#contents_user_actions_questions'));	// 发起的问题
 		
-	bp_more_load(G_BASE_URL + '/people/ajax/user_actions/distint-1__uid-' + PEOPLE_USER_ID + '__actions-' + ACTIVITY_ACTIONS, $('#bp_user_actions_more'), $('#contents_user_actions'));	// 个人动态
+	bp_more_load(G_BASE_URL + '/people/ajax/user_actions/uid-' + PEOPLE_USER_ID + '__actions-' + ACTIVITY_ACTIONS, $('#bp_user_actions_more'), $('#contents_user_actions'));	// 个人动态
 		
 	bp_more_load(G_BASE_URL + '/people/ajax/follows/type-follows__uid-' + PEOPLE_USER_ID, $('#bp_user_follows_more'), $('#contents_user_follows'));	// 关注
 	bp_more_load(G_BASE_URL + '/people/ajax/follows/type-fans__uid-' + PEOPLE_USER_ID, $('#bp_user_fans_more'), $('#contents_user_fans'));	// 粉丝
