ba3ef9970369d0a21068fa46b206018c541cb500
zq <zq@2065f616-0316-476b-a4df-6472c8b1327c>
2013-04-11 10:07:08 +0000

    sssss

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 |   
 +---------------------------------------------------------------------------
 */
-a
+
 if (! file_exists(dirname(__FILE__) . '/system/config/database.php') AND ! file_exists(dirname(__FILE__) . '/system/config/install.lock.php'))
 {
 	header('Location: ./install/');
