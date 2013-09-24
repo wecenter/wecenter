4968d8dae34ff7d9907a8aa6b44637744e07a038
zhengqiang <zhengqiang@2065f616-0316-476b-a4df-6472c8b1327c>
2013-09-24 07:47:35 +0000

    删除static目录

--- static/js/compatibility.js
+++
@@ -1,4 +0,0 @@
-if (/MSIE 6/.test(navigator.userAgent) || /MSIE 7/.test(navigator.userAgent))
-{
-	window.location = G_BASE_URL + '/home/browser_not_support/';
-}
--- static/js/plug_module/loader.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-	$('body').append(decodeURIComponent('%3Cscript%20type%3D%22text%2Fjavascript%22%20src%3D%22') + G_STATIC_URL + decodeURIComponent('%2Fjs%2Fplug_module%2Fplug-in_module.js%22%3E%3C%2Fscript%3E'));
-});
