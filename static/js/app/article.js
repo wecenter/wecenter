c62b9243e35f587b4816c2e4f3b5ce2ad0cc9498
yejunkang <yejunkang@2065f616-0316-476b-a4df-6472c8b1327c>
2013-10-28 06:49:00 +0000

    文章功能

---
+++ static/js/app/article.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+	$.each($('.aw-article-list .aw-item .aw-mod-body .content'), function (i, e)
+	{
+		if ($(this).innerHeight() > parseInt($(this).parents('.content-wrap').css('max-height')))
+		{
+			$(this).parents('.aw-mod-body').find('.more').show();
+		}
+	});
+});
+
