120f73a6dce379a6319e16316112ace95052db2d
yejunkang <yejunkang@2065f616-0316-476b-a4df-6472c8b1327c>
2013-10-31 10:13:45 +0000

    主板本bug修复

--- static/js/app/article.js
+++ static/js/app/article.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
-	$('.aw-article-list .aw-item .aw-mod-body .content-wrap').css('height',parseInt($('.aw-article-list .aw-item .aw-mod-body .content').css('line-height'))*10);
 	$.each($('.aw-article-list .aw-item .aw-mod-body .content'), function (i, e)
 	{
-		// console.log($(this).css('line-height'));
-		if ($(this).innerHeight() > parseInt($(this).parents('.content-wrap').css('max-height')))
+		if ($(this).innerHeight() > parseInt($(this).css('line-height'))*10)
 		{
 			$(this).parents('.aw-content').find('.more').show();
+			$(this).parents('.content-wrap').css('height',parseInt($(this).css('line-height'))*10);
 		}
 	});
 });
