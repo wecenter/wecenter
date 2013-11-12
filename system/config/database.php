22d3a31ef1fcc4a01fe1ea9710df7607756483c3
kk <capri19910730@gmail.com>
2013-11-12 11:21:55 +0800

    no message

---
+++ system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => 'root',
+  'dbname' => 'anwsion',
+);
+$config['slave'] = false;
