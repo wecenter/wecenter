0ebdec82522fe91bf9e12fa64e8db67b2e741f62
kk <capri19910730@gmail.com>
2013-11-13 17:56:22 +0800

    修改.gitignore

---
+++ system/config.inc.php
@@ -0,0 +1,39 @@
+<?php
+/*
++--------------------------------------------------------------------------
+|   Anwsion [#RELEASE_VERSION#]
+|   ========================================
+|   by Anwsion dev team
+|   (c) 2011 - 2012 Anwsion Software
+|   http://www.anwsion.com
+|   ========================================
+|   Support: zhengqiang@gmail.com
+|   
++---------------------------------------------------------------------------
+*/
+
+// 定义 Cookies 作用域
+define('G_COOKIE_DOMAIN','');
+
+// 定义 Cookies 前缀
+define('G_COOKIE_PREFIX','csc_');
+
+// 定义应用加密 KEY
+define('G_SECUKEY','xnksahihodgq');
+define('G_COOKIE_HASH_KEY', 'odtpwdhowspnjfi');
+
+define('G_INDEX_SCRIPT', '?/');
+
+define('X_UA_COMPATIBLE', 'Chrome=1,IE=edge');
+
+// GZIP 压缩输出页面
+define('G_GZIP_COMPRESS', FALSE);
+
+// Session 存储类型 (db, file)
+define('G_SESSION_SAVE', 'db');
+
+// Session 文件存储路径
+define('G_SESSION_SAVE_PATH', '');
+
+// 专家组用户组 ID
+define('SPECIALIST_GROUP_ID', 10);
