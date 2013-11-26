0a36b6cfaaa29654b0a22a7f70f0f3907d8b46c9
explon <explon@gmail.com>
2013-11-26 11:49:58 +0800

    WeChat

--- app/m/weixin.php
+++ app/m/weixin.php
@@ -26,8 +26,10 @@ class weixin extends AWS_CONTROLLER
 {
 	public function get_access_rule()
 	{
-		$rule_action['rule_type'] = 'white';
-		$rule_action['actions'] = array();
+		$rule_action['rule_type'] = 'black';
+		$rule_action['actions'] = array(
+			'binding'	
+		);
 		
 		return $rule_action;
 	}
@@ -50,13 +52,41 @@ class weixin extends AWS_CONTROLLER
 				
 				$access_user = json_decode(curl_get_contents('https://api.weixin.qq.com/sns/userinfo?access_token=' . $access_token['access_token'] . '&openid=' . $access_token['openid']));
 				
+				AWS_APP::session()->WXConnect = array(
+					'access_token' => $access_token,
+					'access_user' => $access_user
+				);
+				
 				TPL::assign('access_token', $access_token);
 				TPL::assign('access_user', $access_user);
 				
 				TPL::output('m/weixin/authorization');
+				die;
 			}
 		}
 		
 		H::redirect_msg(AWS_APP::lang()->_t('授权失败, 请返回重新操作'));
+	}
+	
+	public function binding_action()
+	{
+		if ($weixin_user = $this->model('openid_weixin')->get_user_info_by_openid(AWS_APP::session()->WXConnect['access_token']['openid']))
+		{
+			$user_info = $this->model('account')->get_user_info_by_uid($weixin_user['uid']);
+			
+			HTTP::set_cookie('_user_login', get_login_cookie_hash($user_info['user_name'], $user_info['password'], $user_info['salt'], $user_info['uid'], false));
+			
+			HTTP::redirect('/');
+		}
+		else if (AWS_APP::session()->WXConnect['access_token']['openid'])
+		{
+			$this->model('openid_weixin')->bind_account(AWS_APP::session()->WXConnect['access_user'], AWS_APP::session()->WXConnect['access_token'], $this->user_id);
+			
+			H::redirect_msg(AWS_APP::lang()->_t('绑定微信成功'));
+		}
+		else
+		{
+			H::redirect_msg(AWS_APP::lang()->_t('授权失败, 请返回重新操作'));
+		}
 	}
 }
--- install/db/mysql.sql
+++ install/db/mysql.sql
@@ -813,6 +813,33 @@ CREATE TABLE `[#DB_PREFIX#]users_sina` (
   UNIQUE KEY `uid` (`uid`)
 ) ENGINE=[#DB_ENGINE#] DEFAULT CHARSET=utf8;
 
+CREATE TABLE `[#DB_PREFIX#]users_weixin` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `uid` int(10) NOT NULL,
+  `openid` varchar(255) NOT NULL,
+  `expires_in` int(10) NOT NULL,
+  `access_token` varchar(255) NOT NULL,
+  `refresh_token` varchar(255) NOT NULL,
+  `scope` varchar(64) NOT NULL,
+  `headimgurl` varchar(255) NOT NULL,
+  `nickname` varchar(64) NOT NULL,
+  `sex` tinyint(1) NOT NULL DEFAULT '0',
+  `province` varchar(32) NOT NULL,
+  `city` varchar(32) NOT NULL,
+  `country` varchar(32) NOT NULL,
+  `add_time` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uid` (`uid`),
+  KEY `openid` (`openid`),
+  KEY `expires_in` (`expires_in`),
+  KEY `scope` (`scope`),
+  KEY `sex` (`sex`),
+  KEY `province` (`province`),
+  KEY `city` (`city`),
+  KEY `country` (`country`),
+  KEY `add_time` (`add_time`)
+) ENGINE=[#DB_ENGINE#] DEFAULT CHARSET=utf8;
+
 CREATE TABLE `[#DB_PREFIX#]users_ucenter` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `uid` int(11) DEFAULT '0',
