318b0326205bd66bb16b73d73069e614f84cc84a
kk <capri19910730@gmail.com>
2013-12-03 13:28:08 +0800

    微信屏蔽测试

--- static/js/mobile/mobile.js
+++ static/js/mobile/mobile.js
@@ -59,8 +59,10 @@ $(window).on('hashchange', function() {
 
 $(document).ready(function () {
 	// 判断是否微信打开
+	alert(WeixinJSBridge);
     if (typeof WeixinJSBridge != 'undefined')
     {
+    	alert(WeixinJSBridge);
         $('header, nav, footer').hide();
     }
 
