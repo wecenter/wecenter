af9bf5e528350106e030cc7dd5e82db9df9c8c7d
explon <explon@gmail.com>
2013-12-12 16:14:32 +0800

    微信登录

--- models/openid/weixin.php
+++ models/openid/weixin.php
@@ -40,7 +40,7 @@ class openid_weixin_class extends AWS_MODEL
 			}
 			else
 			{
-				H::redirect_msg(AWS_APP::lang()->_t('与微信通信出错, 请重新登录'), '/account/logout/');
+				H::redirect_msg(AWS_APP::lang()->_t('与微信通信出错, 请重新登录'));
 			}
 		}
 		
@@ -112,7 +112,7 @@ class openid_weixin_class extends AWS_MODEL
 	
 	public function register($access_token, $access_user)
 	{
-		if (!$access_token OR !$access_user)
+		if (!$access_token OR !$access_user['nickname'])
 		{
 			return false;
 		}
@@ -141,7 +141,7 @@ class openid_weixin_class extends AWS_MODEL
 						make_dir($avatar_dir);
 					}
 					
-					if (file_put_contents(get_setting('upload_dir') . '/avatar/' . $this->model('account')->get_avatar($uid, '', 1) . '/' . $this->model('account')->get_avatar($uid, '', 2), $avatar_stream))
+					if (@file_put_contents($avatar_location, $avatar_stream))
 					{
 						foreach(AWS_APP::config()->get('image')->avatar_thumbnail AS $key => $val)
 						{			
