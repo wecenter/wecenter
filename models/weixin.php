81adb2a9efec5f46c132cdc52d00323357fef899
explon <explon@gmail.com>
2013-12-12 15:47:12 +0800

    微信登录

--- models/weixin.php
+++ models/weixin.php
@@ -352,7 +352,7 @@ class weixin_class extends AWS_MODEL
 					{
 						$response_message = $this->bind_message;
 					}
-					else if ($this->model('openid_weixin')->process_client_login(substr($input_message['content'], 4), $this->user_id))
+					else if ($this->model('openid_weixin')->process_client_login(trim(substr($input_message['content'], 4)), $this->user_id))
 					{
 						$response_message = '你已成功登录网站';
 					}
