77506ce740a8ce8415b45bce4020dd285069e127
explon <explon@gmail.com>
2013-12-19 19:06:48 +0800

    微信普通版 Open ID 支持

--- models/openid/weixin.php
+++ models/openid/weixin.php
@@ -32,16 +32,9 @@ class openid_weixin_class extends AWS_MODEL
 	
 	public function bind_account($access_user, $access_token, $uid, $is_ajax = false)
 	{
-		if (! $access_user['nickname'])
+		if (AWS_APP::plugins()->installed('aws_weixin_enterprise'))
 		{
-			if ($is_ajax)
-			{
-				H::ajax_json_output(AWS_APP::RSM(null, -1, AWS_APP::lang()->_t('与微信通信出错, 请重新登录')));
-			}
-			else
-			{
-				H::redirect_msg(AWS_APP::lang()->_t('与微信通信出错, 请重新登录'));
-			}
+			return $this->model('aws_weixin_enterprise')->bind_account($access_user, $access_token, $uid, $is_ajax);
 		}
 		
 		if ($openid_info = $this->get_user_info_by_uid($uid))
@@ -59,33 +52,11 @@ class openid_weixin_class extends AWS_MODEL
 			}
 		}
 		
-		return $this->add_user($uid, $access_user, $access_token);
-	}
-	
-	public function add_user($uid, $access_user, $access_token)
-	{
 		return $this->insert('users_weixin', array(
 			'uid' => intval($uid),
 			'openid' => $access_token['openid'],
-			'expires_in' => (time() + $access_token['expires_in']),
-			'access_token' => $access_token['access_token'],
-			'refresh_token' => $access_token['refresh_token'],
-			'scope' => $access_token['scope'],
-			'headimgurl' => $access_user['headimgurl'],
-			'nickname' => $access_user['nickname'],
-			'sex' => $access_user['sex'],
-			'province' => $access_user['province'],
-			'city' => $access_user['city'],
-			'country' => $access_user['country'],
 			'add_time' => time()
 		));
-	}
-	
-	public function update_token($openid, $access_token)
-	{
-		$this->update('users_weixin', array(
-			'access_token' => $access_token
-		), "openid = '" . $this->quote($openid) . "'");
 	}
 	
 	public function weixin_unbind($uid)
@@ -95,11 +66,6 @@ class openid_weixin_class extends AWS_MODEL
 	
 	public function get_oauth_url($redirect_uri, $scope = 'snsapi_base', $state = 'STATE')
 	{		
-		if (!AWS_APP::plugins()->installed('aws_weixin_enterprise'))
-		{
-			return get_js_url($redirect_uri);
-		}
-		
 		return get_js_url('/m/weixin/oauth_redirect/?uri=' . urlencode($redirect_uri) . '&scope=' . urlencode($scope) . '&state=' . urlencode($state));
 	}
 	
@@ -111,45 +77,6 @@ class openid_weixin_class extends AWS_MODEL
 		}
 		
 		return $this->get_oauth_url(get_js_url('/m/weixin/redirect/?redirect=' . base64_encode(get_js_url($redirect_uri))));
-	}
-	
-	public function process_client_login($token, $uid)
-	{
-		return $this->update('weixin_login', array(
-			'uid' => $uid
-		), "token = '" . intval($token) . "'");
-	}
-	
-	public function request_client_login_token($session_id)
-	{
-		$this->delete('weixin_login', "session_id = '" . $this->quote($session_id) . "'");
-		
-		$token = rand(11111111, 99999999);
-		
-		while ($this->fetch_row('weixin_login', "token = " . $token))
-		{
-			$token = rand(11111111, 99999999);
-		}
-		
-		$this->insert('weixin_login', array(
-			'token' => $token,
-			'session_id' => $session_id,
-			'expire' => (time() + 300)
-		));
-		
-		return $token;
-	}
-	
-	public function weixin_login_process($session_id)
-	{
-		$weixin_login = $this->fetch_row('weixin_login', "session_id = '" . $this->quote($session_id) . "' AND expire >= " . time());
-		
-		if ($weixin_login['uid'])
-		{
-			$this->delete('weixin_login', "session_id = '" . $this->quote($session_id) . "'");
-			
-			return $this->model('account')->get_user_info_by_uid($weixin_login['uid']);
-		}
 	}
 }
 	
