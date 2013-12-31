2f23ce0e28bd5d0cc4b84fe131bf08387640d382
explon <explon@gmail.com>
2014-01-01 00:04:16 +0800

    bugfix

--- api/admin_notify.php
+++ api/admin_notify.php
@@ -1,7 +1,8 @@
 <?php
 
 $notify = <<<EOF
-<li><a href="http://www.anwsion.com/downloads/" target="_blank">WeCenter 2.2.6 发布</a></li>
+<li><a href="http://wenda.wecenter.com/question/13876" target="_blank">WeCenter 2.5 BETA 5 发布</a></li>
+<li><a href="http://www.anwsion.com/downloads/" target="_blank">WeCenter 2.2.7 发布</a></li>
 EOF;
 
 $notify = str_replace(array("\n", "\r", "\t"), '', $notify);
