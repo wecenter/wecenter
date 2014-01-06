9ffeab15a44735cee08ee635f2e3d2d3c9e84e37
explon <explon@gmail.com>
2014-01-06 20:02:13 +0800

    微信服务调整

--- models/wecenter.php
+++ models/wecenter.php
@@ -22,7 +22,7 @@ class wecenter_class extends AWS_MODEL
 {
 	public function mp_server_query($node, $post_data = null)
 	{
-		if (!AWS_APP::config()->get('wecenter')->mp_access_token)
+		if (get_setting('wecenter_access_token'))
 		{
 			return false;
 		}
@@ -40,22 +40,22 @@ class wecenter_class extends AWS_MODEL
 			return false;
 		}
 		
-		$_post_data[] = 'access_token=' . AWS_APP::config()->get('wecenter')->mp_access_token;
+		$_post_data[] = 'access_token=' . get_setting('wecenter_access_token');
 		$_post_data[] = 'version=1';
 		
 		$curl = curl_init();
 		
-		curl_setopt($curl, CURLOPT_URL, AWS_APP::config()->get('wecenter')->mp_server . $node . '/');
+		curl_setopt($curl, CURLOPT_URL, 'http://mp.wecenter.com/?/' . $node . '/');
 		curl_setopt($curl, CURLOPT_HEADER, false);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		
 		curl_setopt($curl, CURLOPT_POST, true);
 		curl_setopt($curl, CURLOPT_POSTFIELDS, implode('&', $_post_data));
 		
-		$content = curl_exec($curl);
+		$content = trim(curl_exec($curl));
 		
 		curl_close($curl);
 		
-		return json_decode(trim($content), true);
+		return json_decode($content, true);
 	}
 }
