f509036e4e1b2d42f22ba37fa715d97d9c39919f
explon <explon@gmail.com>
2014-01-24 22:01:06 +0800

    bugfix

--- api/admin_notify.php
+++ api/admin_notify.php
@@ -1,8 +1,8 @@
 <?php
 
 $notify = <<<EOF
-<li><a href="http://wenda.wecenter.com/question/13876" target="_blank">WeCenter 2.5 BETA 5 发布</a></li>
-<li><a href="http://www.anwsion.com/downloads/" target="_blank">WeCenter 2.2.7 发布</a></li>
+<li><a href="http://wenda.wecenter.com/question/14296" target="_blank">WeCenter 2.5 正式版发布</a></li>
+<li><a href="http://wenda.wecenter.com/question/14310" target="_blank">WeCenter 2.5 IE8 补丁</a></li>
 EOF;
 
 $notify = str_replace(array("\n", "\r", "\t"), '', $notify);
