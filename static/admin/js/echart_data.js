3c66f3e9945f33beadf2c9d5730421aa81c8f9ef
taoche <taowei@taoweitekiMacBook-Pro.local>
2014-06-04 17:57:20 +0800

    no message

--- static/admin/js/echart_data.js
+++ static/admin/js/echart_data.js
@@ -43,256 +43,373 @@ function oData() {
 
         animation:false,
         addDataAnimation:false,
-
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            padding: 5,
-            itemGap: 10,
-            data: ['蒸发量', '降水量']
-        },
         grid:{
            x:45,
-           y:40,
+           y:25,
            x2:15,
-           y2:30,
-           backgroundColor:'#fff'
-        },
-        toolbox: {
-            show: true,
-            feature: {
-                restore: {show: true},
-                magicType: {show: true, type: ['line', 'bar']}
-            }
+           y2:35,
+           backgroundColor:'#fff',
+           borderColor: '#fff'
         },
         calculable: true,
         xAxis: [
             {
                 type: 'category',
+                splitLine : {
+                    show:false,
+                },
+
+                axisLine : {    
+                    show: false
+                },
+                axisTick : {    
+                    show:false,
+                },
                 data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
             }
         ],
         yAxis: [
             {
                 type: 'value',
-                splitArea: {show: true}
+                 splitLine : {
+                    show:false,
+                },
+
+                axisLine : {    
+                    show: false
+                },
+
+                splitLine : {
+                    show:true,
+                    lineStyle: {
+                        color: 'rgba(0,0,0,0.1)',
+                        type: 'dashed',
+                        width: 1
+                    }
+                },
+
+                splitArea : {
+                    show: false,
+                }
             }
         ],
+
         series: [
             {
                 name: '蒸发量',
                 type: 'line',
+                symbol: 'none',
+                itemStyle: {
+                     normal: {
+                          lineStyle:{
+                            width:4,
+                            color:'#3695d5',
+                          }
+                        },
+                },
                 data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
             },
-            {
+             {
                 name: '降水量',
                 type: 'line',
+                symbol: 'none',
+                itemStyle: {
+                     normal: {
+                          lineStyle:{
+                            width:4,
+                            color:'red',
+                          }
+                        },
+                },
                 data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
             }
+
+
         ]
     });
+    
+
     myChart2.setOption({
         animation:false,
         addDataAnimation:false,
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            padding: 5,
-            itemGap: 10,
-            data: ['蒸发量', '降水量']
-        },
         grid:{
-            x:45,
-            y:40,
-            x2:15,
-            y2:30,
-            backgroundColor:'#fff'
-        },
-        toolbox: {
-            show: true,
-            feature: {
-                restore: {show: true},
-                magicType: {show: true, type: ['line', 'bar']}
-            }
+           x:45,
+           y:25,
+           x2:15,
+           y2:35,
+           backgroundColor:'#fff',
+           borderColor: '#fff'
         },
         calculable: true,
         xAxis: [
             {
                 type: 'category',
+                splitLine : {
+                    show:false,
+                },
+
+                axisLine : {    
+                    show: false
+                },
+                axisTick : {    
+                    show:false,
+                },
                 data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
             }
         ],
         yAxis: [
             {
                 type: 'value',
-                splitArea: {show: true}
+                 splitLine : {
+                    show:false,
+                },
+
+                axisLine : {    
+                    show: false
+                },
+
+                splitLine : {
+                    show:true,
+                    lineStyle: {
+                        color: 'rgba(0,0,0,0.1)',
+                        type: 'dashed',
+                        width: 1
+                    }
+                },
+
+                splitArea : {
+                    show: false,
+                }
             }
         ],
+
         series: [
-            {
-                name: '蒸发量',
-                type: 'line',
-                data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-            },
             {
                 name: '降水量',
                 type: 'line',
-                data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+                symbol: 'none',
+                itemStyle: {
+                     normal: {
+                          lineStyle:{
+                            width:3,
+                            color:'#3695d5',
+                          }
+                        },
+                },
+                data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
             }
         ]
     });
     myChart3.setOption({
         animation:false,
         addDataAnimation:false,
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            padding: 5,
-            itemGap: 10,
-            data: ['蒸发量', '降水量']
-        },
         grid:{
-            x:45,
-            y:40,
-            x2:15,
-            y2:30,
-            backgroundColor:'#fff'
-        },
-        toolbox: {
-            show: true,
-            feature: {
-                restore: {show: true},
-                magicType: {show: true, type: ['line', 'bar']}
-            }
+           x:45,
+           y:25,
+           x2:15,
+           y2:35,
+           backgroundColor:'#fff',
+           borderColor: '#fff'
         },
         calculable: true,
         xAxis: [
             {
                 type: 'category',
+                splitLine : {
+                    show:false,
+                },
+
+                axisLine : {    
+                    show: false
+                },
+                axisTick : {    
+                    show:false,
+                },
                 data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
             }
         ],
         yAxis: [
             {
                 type: 'value',
-                splitArea: {show: true}
+                 splitLine : {
+                    show:false,
+                },
+
+                axisLine : {    
+                    show: false
+                },
+
+                splitLine : {
+                    show:true,
+                    lineStyle: {
+                        color: 'rgba(0,0,0,0.1)',
+                        type: 'dashed',
+                        width: 1
+                    }
+                },
+
+                splitArea : {
+                    show: false,
+                }
             }
         ],
+
         series: [
-            {
-                name: '蒸发量',
-                type: 'line',
-                data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-            },
             {
                 name: '降水量',
                 type: 'line',
-                data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+                symbol: 'none',
+                itemStyle: {
+                     normal: {
+                          lineStyle:{
+                            width:3,
+                            color:'#3695d5',
+                          }
+                        },
+                },
+                data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
             }
         ]
     });
     myChart4.setOption({
         animation:false,
         addDataAnimation:false,
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            padding: 5,
-            itemGap: 10,
-            data: ['蒸发量', '降水量']
-        },
         grid:{
-            x:45,
-            y:40,
-            x2:15,
-            y2:30,
-            backgroundColor:'#fff'
-        },
-        toolbox: {
-            show: true,
-            feature: {
-                restore: {show: true},
-                magicType: {show: true, type: ['line', 'bar']}
-            }
+           x:45,
+           y:25,
+           x2:15,
+           y2:35,
+           backgroundColor:'#fff',
+           borderColor: '#fff'
         },
         calculable: true,
         xAxis: [
             {
                 type: 'category',
+                splitLine : {
+                    show:false,
+                },
+
+                axisLine : {    
+                    show: false
+                },
+                axisTick : {    
+                    show:false,
+                },
                 data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
             }
         ],
         yAxis: [
             {
                 type: 'value',
-                splitArea: {show: true}
+                 splitLine : {
+                    show:false,
+                },
+
+                axisLine : {    
+                    show: false
+                },
+
+                splitLine : {
+                    show:true,
+                    lineStyle: {
+                        color: 'rgba(0,0,0,0.1)',
+                        type: 'dashed',
+                        width: 1
+                    }
+                },
+
+                splitArea : {
+                    show: false,
+                }
             }
         ],
+
         series: [
-            {
-                name: '蒸发量',
-                type: 'line',
-                data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-            },
             {
                 name: '降水量',
                 type: 'line',
-                data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+                symbol: 'none',
+                itemStyle: {
+                     normal: {
+                          lineStyle:{
+                            width:3,
+                            color:'#3695d5',
+                          }
+                        },
+                },
+                data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
             }
         ]
     });
     myChart5.setOption({
         animation:false,
         addDataAnimation:false,
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            padding: 5,
-            itemGap: 10,
-            data: ['蒸发量', '降水量']
-        },
         grid:{
-            x:45,
-            y:40,
-            x2:15,
-            y2:30,
-            backgroundColor:'#fff'
-        },
-        toolbox: {
-            show: true,
-            feature: {
-                restore: {show: true},
-                magicType: {show: true, type: ['line', 'bar']}
-            }
+           x:45,
+           y:25,
+           x2:15,
+           y2:35,
+           backgroundColor:'#fff',
+           borderColor: '#fff'
         },
         calculable: true,
         xAxis: [
             {
                 type: 'category',
+                splitLine : {
+                    show:false,
+                },
+
+                axisLine : {    
+                    show: false
+                },
+                axisTick : {    
+                    show:false,
+                },
                 data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
             }
         ],
         yAxis: [
             {
                 type: 'value',
-                splitArea: {show: true}
+                 splitLine : {
+                    show:false,
+                },
+
+                axisLine : {    
+                    show: false
+                },
+
+                splitLine : {
+                    show:true,
+                    lineStyle: {
+                        color: 'rgba(0,0,0,0.1)',
+                        type: 'dashed',
+                        width: 1
+                    }
+                },
+
+                splitArea : {
+                    show: false,
+                }
             }
         ],
+
         series: [
-            {
-                name: '蒸发量',
-                type: 'line',
-                data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-            },
             {
                 name: '降水量',
                 type: 'line',
-                data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+                symbol: 'none',
+                itemStyle: {
+                     normal: {
+                          lineStyle:{
+                            width:3,
+                            color:'#3695d5',
+                          }
+                        },
+                },
+                data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
             }
         ]
     });
