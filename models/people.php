f9d7ed8bd7dc6d2707637caf0d5047b798801407
explon <explon@gmail.com>
2014-06-27 10:36:34 +0800

    bugfix

--- models/people.php
+++ models/people.php
@@ -22,12 +22,12 @@ class people_class extends AWS_MODEL
 {	
 	public function update_views($uid)
 	{
-		if (AWS_APP::cache()->get('update_views_people_' . md5(session_id()) . '_' . intval($question_id)))
+		if (AWS_APP::cache()->get('update_views_people_' . md5(session_id()) . '_' . intval($uid)))
 		{
 			return false;
 		}
 		
-		AWS_APP::cache()->set('update_views_people_' . md5(session_id()) . '_' . intval($question_id), time(), get_setting('cache_level_normal'));
+		AWS_APP::cache()->set('update_views_people_' . md5(session_id()) . '_' . intval($uid), time(), get_setting('cache_level_normal'));
 		
 		return $this->query('UPDATE ' . $this->get_table('users') . ' SET views_count = views_count + 1 WHERE uid = ' . intval($uid));
 	}
