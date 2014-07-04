97ecc6bd20d6e003a5b5cdfc6b04214fc342bd51
explon <explon@gmail.com>
2014-07-04 18:43:24 +0800

    新版本提示推送

--- api/admin_notify.php
+++ api/admin_notify.php
@@ -1,7 +1,7 @@
 <?php
 
 $notify = <<<EOF
-<li><a href="http://wenda.wecenter.com/question/16966" target="_blank">WeCenter 2.5.10 发布</a></li>
+<li><a href="http://wenda.wecenter.com/question/18106" target="_blank">WeCenter 2.5.15 发布</a></li>
 EOF;
 
 $notify = str_replace(array("\n", "\r", "\t"), '', $notify);
