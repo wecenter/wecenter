dbd3666b16fb9100534c8ae7fe69117c1088bdae
Jat <dpjat1@gmail.com>
2014-07-18 00:06:48 +0800

    update parent topics

--- app/admin/user.php
+++ app/admin/user.php
@@ -109,20 +109,8 @@ class user extends AWS_ADMIN_CONTROLLER
 
         $total_rows = $this->model('people')->found_rows();
 
-        $url_param = array();
-
-        $url_param[] = 'type-' . $_GET['type'];
-
-        foreach($_GET as $key => $val)
-        {
-            if (!in_array($key, array('app', 'c', 'act', 'page')))
-            {
-                $url_param[] = $key . '-' . $val;
-            }
-        }
-
         TPL::assign('pagination', AWS_APP::pagination()->initialize(array(
-            'base_url' => get_js_url('/admin/user/list/' . implode('__', $url_param)),
+            'base_url' => get_js_url('/admin/user/list/'),
             'total_rows' => $total_rows,
             'per_page' => $this->per_page
         ))->create_links());
