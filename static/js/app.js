a566d7b7f12409f7ceb984532b58c6b4f679c72f
kk <capri19910730@gmail.com>
2014-07-23 16:31:24 +0800

    bugfix

--- static/js/app.js
+++ static/js/app.js
@@ -2,9 +2,6 @@ var document_title = document.title;
 
 $(document).ready(function ()
 {
-    // 文章截断显示更多
-    
-    
     // fix form bug...
     $("form[action='']").attr('action', window.location.href);
 
