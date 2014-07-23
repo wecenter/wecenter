34dda448cdeebc1f65e055d5e3923a52f3379454
Jat <dpjat1@gmail.com>
2014-07-23 15:55:17 +0800

    bug fix

--- app/admin/ajax.php
+++ app/admin/ajax.php
@@ -944,10 +944,10 @@ class ajax extends AWS_ADMIN_CONTROLLER
     {
         if (!$_POST['topic_ids'])
         {
-            H::ajax_json_output(AWS_APP::RSM(nul, -1, AWS_APP::lang()->_t('请选择话题进行操作')));
+            H::ajax_json_output(AWS_APP::RSM(null, -1, AWS_APP::lang()->_t('请选择话题进行操作')));
         }
 
-        switch($_POST['action_type'])
+        switch($_POST['action'])
         {
             case 'remove' :
                 $this->model('topic')->remove_topic_by_ids($_POST['topic_ids']);
@@ -957,7 +957,6 @@ class ajax extends AWS_ADMIN_CONTROLLER
                 $this->model('topic')->lock_topic_by_ids($_POST['topic_ids'], 1);
             break;
         }
-
 
         H::ajax_json_output(AWS_APP::RSM(null, 1, null));
     }
@@ -1962,7 +1961,7 @@ class ajax extends AWS_ADMIN_CONTROLLER
 
                     $this->model('weibo')->update_service_account($user_info['uid'], 'add');
 
-                    $rsm = array('satus' => 'bound');
+                    $rsm = array('staus' => 'bound');
                 }
                 else
                 {
@@ -1981,7 +1980,7 @@ class ajax extends AWS_ADMIN_CONTROLLER
 
                     AWS_APP::cache()->set('tmp_service_account', $tmp_service_account, 86400);
 
-                    $rsm = array('satus' => 'unbound');
+                    $rsm = array('staus' => 'unbound');
                 }
 
                 break;
