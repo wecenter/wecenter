379a6a4a4073d2b2904c6571d55442cf5cba7188
Jat <dpjat1@gmail.com>
2014-08-11 16:53:53 +0800

    bug fix

--- app/admin/ajax.php
+++ app/admin/ajax.php
@@ -2444,7 +2444,7 @@ class ajax extends AWS_ADMIN_CONTROLLER
             H::ajax_json_output(AWS_APP::RSM(null, -1, AWS_APP::lang()->_t('请输入关键词')));
         }
 
-        if (!$_POST['url'] OR substr($_POST['url'], 0, 7) != 'http://' OR substr($_POST['url'], 0, 8) != 'https://')
+        if (!$_POST['url'] OR substr($_POST['url'], 0, 7) != 'http://' AND substr($_POST['url'], 0, 8) != 'https://')
         {
             H::ajax_json_output(AWS_APP::RSM(null, -1, AWS_APP::lang()->_t('请输入正确的 URL')));
         }
