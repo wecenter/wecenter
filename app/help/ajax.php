6101049b4c7ff2ce0f706a217d799403b4b52047
Jat <dpjat1@gmail.com>
2014-10-13 15:34:13 +0800

    help center

---
+++ app/help/ajax.php
@@ -0,0 +1,91 @@
+<?php
+/*
++--------------------------------------------------------------------------
+|   WeCenter [#RELEASE_VERSION#]
+|   ========================================
+|   by WeCenter Software
+|   © 2011 - 2014 WeCenter. All Rights Reserved
+|   http://www.wecenter.com
+|   ========================================
+|   Support: WeCenter@qq.com
+|
++---------------------------------------------------------------------------
+*/
+
+define('IN_AJAX', TRUE);
+
+if (!defined('IN_ANWSION'))
+{
+    die;
+}
+
+class ajax extends AWS_CONTROLLER
+{
+    public function get_access_rule()
+    {
+        $rule_action['rule_type'] = "white"; //'black'黑名单,黑名单中的检查  'white'白名单,白名单以外的检查
+
+        $rule_action['actions'] = array();
+
+        return $rule_action;
+    }
+
+    public function setup()
+    {
+        HTTP::no_cache_header();
+
+        if (!$this->user_info['permission']['is_administortar'] AND !$this->user_info['permission']['is_moderator'])
+        {
+            H::ajax_json_output(AWS_APP::RSM(null, -1, AWS_APP::lang()->_t('你没有权限进行此操作')));
+        }
+    }
+
+    public function add_action()
+    {
+        if (!$_POST['id'] OR !$_POST['type'] OR !$_POST['item_id'])
+        {
+            H::ajax_json_output(AWS_APP::RSM(null, -1, AWS_APP::lang()->_t('错误的请求')));
+        }
+
+        $chapter_info = $this->model('chapter')->get_chapter_by_id($_POST['id']);
+
+        if (!$chapter_info)
+        {
+            H::ajax_json_output(AWS_APP::RSM(null, -1, AWS_APP::lang()->_t('指定章节不存在')));
+        }
+
+        switch ($_POST['type'])
+        {
+            case 'question':
+                $question_info = $this->model('question')->get_question_info_by_id($_POST['item_id']);
+
+                if (!$question_info)
+                {
+                    H::ajax_json_output(AWS_APP::RSM(null, -1, AWS_APP::lang()->_t('指定问题不存在')));
+                }
+
+                $this->model('chapter')->add_data($chapter_info['id'], 'question', $question_info['question_id']);
+
+                break;
+
+            case 'article':
+                $article_info =  $this->model('article')->get_article_info_by_id($_POST['item_id']);
+
+                if (!$article_info)
+                {
+                    H::ajax_json_output(AWS_APP::RSM(null, -1, AWS_APP::lang()->_t('指定文章不存在')));
+                }
+
+                $this->model('chapter')->add_data($chapter_info['id'], 'article', $article_info['id']);
+
+                break;
+
+            default:
+                H::ajax_json_output(AWS_APP::RSM(null, -1, AWS_APP::lang()->_t('错误的请求')));
+
+                break;
+        }
+
+        H::ajax_json_output(AWS_APP::RSM(null, 1, null));
+    }
+}
