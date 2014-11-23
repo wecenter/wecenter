19ae8b8fa2c39bd883d69c90078a5da6d321ad2b
cesar1134 <cesar.x.avendano@gmail.com>
2014-11-23 13:54:22 -0600

    Update notification.php
    simpler translation

--- system/config/notification.php
+++ system/config/notification.php
@@ -3,125 +3,125 @@
 $config['action_details'][notify_class::TYPE_PEOPLE_FOCUS] = array(
 	'user_setting' => 1,
 	'combine' => 0,
-	'desc' => '有人关注了我'
+	'desc' => AWS_APP::lang()->_t('有人关注了我')
 );
 
 $config['action_details'][notify_class::TYPE_NEW_ANSWER] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '我关注的问题有了新的回复'
+	'desc' => AWS_APP::lang()->_t('我关注的问题有了新的回复')
 );
 
 $config['action_details'][notify_class::TYPE_INVITE_QUESTION] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '有人邀请我回复问题'
+	'desc' => AWS_APP::lang()->_t('有人邀请我回复问题')
 );
 
 $config['action_details'][notify_class::TYPE_QUESTION_COMMENT] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '我的问题被评论'
+	'desc' => AWS_APP::lang()->_t('我的问题被评论')
 );
 
 $config['action_details'][notify_class::TYPE_ANSWER_COMMENT] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '我的问题评论被回复'
+	'desc' => AWS_APP::lang()->_t('我的问题评论被回复')
 );
 
 $config['action_details'][notify_class::TYPE_COMMENT_AT_ME] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '有问题评论提到我'
+	'desc' => AWS_APP::lang()->_t('有问题评论提到我')
 );
 
 $config['action_details'][notify_class::TYPE_ANSWER_COMMENT_AT_ME] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '有回答评论提到我'
+	'desc' => AWS_APP::lang()->_t('有回答评论提到我')
 );
 
 $config['action_details'][notify_class::TYPE_ANSWER_AT_ME] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '有回答提到我'
+	'desc' => AWS_APP::lang()->_t('有回答提到我')
 );
 
 $config['action_details'][notify_class::TYPE_ANSWER_AGREE] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '我的回复收到赞同'
+	'desc' => AWS_APP::lang()->_t('我的回复收到赞同')
 );
 
 $config['action_details'][notify_class::TYPE_ANSWER_THANK] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '我的回复收到感謝'
+	'desc' => AWS_APP::lang()->_t('我的回复收到感謝')
 );
 
 $config['action_details'][notify_class::TYPE_QUESTION_THANK] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '我发布的问题收到感謝'
+	'desc' => AWS_APP::lang()->_t('我发布的问题收到感謝')
 );
 
 $config['action_details'][notify_class::TYPE_MOD_QUESTION] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '我的问题被编辑'
+	'desc' => AWS_APP::lang()->_t('我的问题被编辑')
 );
 
 $config['action_details'][notify_class::TYPE_REMOVE_ANSWER] = array(
 	'user_setting' => 0,
 	'combine' => 1,
-	'desc' => '我发表的回复被删除'
+	'desc' => AWS_APP::lang()->_t('我发表的回复被删除')
 );
 
 $config['action_details'][notify_class::TYPE_REDIRECT_QUESTION] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '我发布的问题被重定向'
+	'desc' => AWS_APP::lang()->_t('我发布的问题被重定向')
 );
 
 $config['action_details'][notify_class::TYPE_CONTEXT] = array(
 	'user_setting' => 0,
 	'combine' => 0,
-	'desc' => '文字通知'
+	'desc' => AWS_APP::lang()->_t('文字通知')
 );
 
 $config['action_details'][notify_class::TYPE_ARTICLE_NEW_COMMENT] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '我的文章被评论'
+	'desc' => AWS_APP::lang()->_t('我的文章被评论')
 );
 
 $config['action_details'][notify_class::TYPE_ARTICLE_COMMENT_AT_ME] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => '有文章评论提到我'
+	'desc' => AWS_APP::lang()->_t('有文章评论提到我')
 );
 
 $config['action_details'][notify_class::TYPE_ARTICLE_APPROVED] = array(
 	'user_setting' => 0,
 	'combine' => 0,
-	'desc' => '文章通过审核'
+	'desc' => AWS_APP::lang()->_t('文章通过审核')
 );
 
 $config['action_details'][notify_class::TYPE_ARTICLE_REFUSED] = array(
 	'user_setting' => 0,
 	'combine' => 0,
-	'desc' => '文章未通过审核'
+	'desc' => AWS_APP::lang()->_t('文章未通过审核')
 );
 
 $config['action_details'][notify_class::TYPE_QUESTION_APPROVED] = array(
 	'user_setting' => 0,
 	'combine' => 0,
-	'desc' => '问题通过审核'
+	'desc' => AWS_APP::lang()->_t('问题通过审核')
 );
 
 $config['action_details'][notify_class::TYPE_QUESTION_REFUSED] = array(
 	'user_setting' => 0,
 	'combine' => 0,
-	'desc' => '问题未通过审核'
+	'desc' => AWS_APP::lang()->_t('问题未通过审核')
 );
