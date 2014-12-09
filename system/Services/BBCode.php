8c9b2b3d6bc1238cc2da77aa41d42caddef1b9af
explon <explon@gmail.com>
2014-12-09 15:10:34 +0800

    BBCode 解析

--- system/Services/BBCode.php
+++ system/Services/BBCode.php
@@ -2,57 +2,108 @@
 
 class Services_BBCode
 {
-    protected $bbcode_table = array();
+	protected $bbcode_table = array();
+    
+	private function _code_callback($match)
+	{
+		return "<pre><code>" . str_replace('[', '<span>[</span>', $match[1]) . "</code></pre>";
+	}
+	
+	private function _b_callback($match)
+	{
+		return "<strong>$match[1]</strong>";
+	}
+	
+	private function _i_callback($match)
+	{
+		return "<em>$match[1]</em>";
+	}
+	
+	private function _quote_callback($match)
+	{
+		return "<blockquote><p>$match[1]</p></blockquote>";
+	}
+	
+	private function _size_callback($match)
+	{
+		return "<span style=\"font-size:$match[1]%\">$match[2]</span>";
+	}
+	
+	private function _s_callback($match)
+	{
+		return "<del>$match[1]</del>";
+	}
+	
+	private function _u_callback($match)
+	{
+		return '<span style="text-decoration:underline;">' . $match[1] . '</span>';
+	}
+	
+	private function _url_callback($match)
+	{
+		return "<a href=\"$match[1]\" rel=\"nofollow\" target=\"_blank\">$match[1]</a>";
+	}
+	
+	private function _link_callback($match)
+	{
+		return "<a href=\"$match[1]\" rel=\"nofollow\" target=\"_blank\">$match[2]</a>";
+	}
+	
+	private function _img_callback($match)
+	{
+		return "<img src=\"$match[1]\" />";
+	}
+	
+	private function _list_callback($match)
+	{
+		$match[1] = preg_replace_callback("/\[\*\](.*?)\[\/\*\]/is", array(&$this, '_list_element_callback'), $match[1]);
+        
+        return "<ul>" . preg_replace("/[\n\r?]/", "", $match[1]) . "</ul>";
+	}
+	
+	private function _list_element_callback($match)
+	{
+		return "<li>" . preg_replace("/[\n\r?]$/", "", $match[1]) . "</li>";
+	}
+	
+	private function _list_advance_callback($match)
+	{
+		if ($match[1] == '1')
+        {
+            $list_type = 'ol';
+        }
+        else
+        { 
+        	$list_type = 'ul';
+        }
+        
+        $match[2] = preg_replace_callback("/\[\*\](.*?)\[\/\*\]/is", array(&$this, '_list_element_callback'), $match[2]);
+        
+        return '<' . $list_type . '>' . preg_replace("/[\n\r?]/", "", $match[2]) . '</' . $list_type . '>';
+	}
 	
     public function __construct()
     {
 	    // Replace [code]...[/code] with <pre><code>...</code></pre>
-        $this->bbcode_table["/\[code\](.*?)\[\/code\]/is"] = function ($match)
-        {
-            return "<pre><code>" . str_replace('[', '<span>[</span>', $match[1]) . "</code></pre>";
-        };
+        $this->bbcode_table["/\[code\](.*?)\[\/code\]/is"] = '_code_callback';
 	    
         // Replace [b]...[/b] with <strong>...</strong>
-        $this->bbcode_table["/\[b\](.*?)\[\/b\]/is"] = function ($match)
-        {
-            return "<strong>$match[1]</strong>";
-        };
+        $this->bbcode_table["/\[b\](.*?)\[\/b\]/is"] = '_b_callback';
         
         // Replace [i]...[/i] with <em>...</em>
-        $this->bbcode_table["/\[i\](.*?)\[\/i\]/is"] = function ($match)
-        {
-            return "<em>$match[1]</em>";
-        };
+        $this->bbcode_table["/\[i\](.*?)\[\/i\]/is"] = '_i_callback';
         
         // Replace [quote]...[/quote] with <blockquote><p>...</p></blockquote>
-        $this->bbcode_table["/\[quote\](.*?)\[\/quote\]/is"] = function ($match)
-        {
-            return "<blockquote><p>$match[1]</p></blockquote>";
-        };
-        
-        // Replace [quote="person"]...[/quote] with <blockquote><p>...</p></blockquote>
-        /*$this->bbcode_table["/\[quote=\"([^\"]+)\"\](.*?)\[\/quote\]/is"] = function ($match)
-        {
-            return "$match[1] wrote: <blockquote><p>$match[2]</p></blockquote>";
-        };*/
+        $this->bbcode_table["/\[quote\](.*?)\[\/quote\]/is"] = '_quote_callback';
         
         // Replace [size=30]...[/size] with <span style="font-size:30%">...</span>
-        $this->bbcode_table["/\[size=(\d+)\](.*?)\[\/size\]/is"] = function ($match)
-        {
-            return "<span style=\"font-size:$match[1]%\">$match[2]</span>";
-        };
+        $this->bbcode_table["/\[size=(\d+)\](.*?)\[\/size\]/is"] = '_size_callback';
         
         // Replace [s] with <del>
-        $this->bbcode_table["/\[s\](.*?)\[\/s\]/is"] = function ($match)
-        {
-            return "<del>$match[1]</del>";
-        };
+        $this->bbcode_table["/\[s\](.*?)\[\/s\]/is"] = '_s_callback';
         
         // Replace [u]...[/u] with <span style="text-decoration:underline;">...</span>
-        $this->bbcode_table["/\[u\](.*?)\[\/u\]/is"] = function ($match)
-        {
-            return '<span style="text-decoration:underline;">' . $match[1] . '</span>';
-        };
+        $this->bbcode_table["/\[u\](.*?)\[\/u\]/is"] = '_u_callback';
         
         // Replace [color=somecolor]...[/color] with <span style="color:somecolor">...</span>
         /*$this->bbcode_table["/\[color=([#a-z0-9]+)\](.*?)\[\/color\]/is"] = function ($match)
@@ -60,73 +111,21 @@ class Services_BBCode
             return '<span style="color:' . $match[1] . ';">' . $match[2] . '</span>';
         };*/
         
-        // Replace [email]...[/email] with <a href="mailto:...">...</a>
-        /*$this->bbcode_table["/\[email\](.*?)\[\/email\]/is"] = function ($match)
-        {
-            return "<a href=\"mailto:$match[1]\">$match[1]</a>";
-        };*/
-        
-        // Replace [email=someone@somewhere.com]An e-mail link[/email] with <a href="mailto:someone@somewhere.com">An e-mail link</a>
-        /*$this->bbcode_table["/\[email=(.*?)\](.*?)\[\/email\]/is"] = function ($match)
-        {
-            return "<a href=\"mailto:$match[1]\">$match[2]</a>";
-        };*/
         
         // Replace [url]...[/url] with <a href="...">...</a>
-        $this->bbcode_table["/\[url\](.*?)\[\/url\]/is"] = function ($match)
-        {
-            return "<a href=\"$match[1]\">$match[1]</a>";
-        };
+        $this->bbcode_table["/\[url\](.*?)\[\/url\]/is"] = '_url_callback';
         
         // Replace [url=http://www.google.com/]A link to google[/url] with <a href="http://www.google.com/">A link to google</a>
-        $this->bbcode_table["/\[url=(.*?)\](.*?)\[\/url\]/is"] = function ($match)
-        {
-            return "<a href=\"$match[1]\">$match[2]</a>";
-        };
+        $this->bbcode_table["/\[url=(.*?)\](.*?)\[\/url\]/is"] = '_link_callback';
         
         // Replace [img]...[/img] with <img src="..."/>
-        $this->bbcode_table["/\[img\](.*?)\[\/img\]/is"] = function ($match)
-        {
-            return "<img src=\"$match[1]\"/>";
-        };
+        $this->bbcode_table["/\[img\](.*?)\[\/img\]/is"] = '_img_callback';
         
         // Replace [list]...[/list] with <ul><li>...</li></ul>
-        $this->bbcode_table["/\[list\](.*?)\[\/list\]/is"] = function ($match)
-        {
-            $match[1] = preg_replace_callback("/\[\*\](.*?)\[\/\*\]/is", function ($submatch)
-            {
-                return "<li>" . preg_replace("/[\n\r?]$/", "", $submatch[1]) . "</li>";
-            }, $match[1]);
-            
-            return "<ul>" . preg_replace("/[\n\r?]/", "", $match[1]) . "</ul>";
-        };
+        $this->bbcode_table["/\[list\](.*?)\[\/list\]/is"] = '_list_callback';
         
         // Replace [list=1|a]...[/list] with <ul|ol><li>...</li></ul|ol>
-        $this->bbcode_table["/\[list=(1|a)\](.*?)\[\/list\]/is"] = function ($match)
-        {
-            if ($match[1] == '1')
-            {
-                $list_type = '<ol>';
-            }
-            else
-            { 
-                if ($match[1] == 'a')
-                {
-                    $list_type = '<ol style="list-style-type: lower-alpha">';
-                }
-                else
-                {
-                    $list_type = '<ol>';
-                }
-            }
-            
-            $match[2] = preg_replace_callback("/\[\*\](.*?)\[\/\*\]/is", function ($submatch)
-            {
-                return "<li>" . preg_replace("/[\n\r?]$/", "", $submatch[1]) . "</li>";
-            }, $match[2]);
-            
-            return $list_type . preg_replace("/[\n\r?]/", "", $match[2]) . "</ol>";
-        };
+        $this->bbcode_table["/\[list=(1|a)\](.*?)\[\/list\]/is"] = '_list_advance_callback';
         
         return $this;
     }
@@ -145,14 +144,12 @@ class Services_BBCode
         
         foreach ($this->bbcode_table AS $key => $val)
         {
-            $text = preg_replace_callback($key, $val, $text);
+            $text = preg_replace_callback($key, array(&$this, $val), $text);
         }
         
         if ($nr2br)
         {
-            $text = preg_replace_callback("/\n\r?/", function ($match) {
-                return "<br />";
-            }, $text);
+            $text = nl2br($text);
         }
         
         return $text;
