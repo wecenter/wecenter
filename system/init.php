9bba28d6818042d946aff6f53ccda5c13fadd370
explon <explon@gmail.com>
2014-12-10 16:58:33 +0800

    调整 PHP 5.4+ error_reporting

--- system/init.php
+++ system/init.php
@@ -53,7 +53,14 @@ if (defined('IN_SAE'))
 }
 else
 {
-	error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
+	if (version_compare(PHP_VERSION, '5.3', '>='))
+	{
+		error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
+	}
+	else
+	{
+		error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
+	}
 
 	define('TEMP_PATH', dirname(dirname(__FILE__)) . '/tmp/');
 }
