3167091871114c3e3d2b2bde5b73141e9e334984
kk <capri19910730@gmail.com>
2014-12-29 15:01:24 +0800

    编辑器压缩

--- static/js/editor/ckeditor/samples/sample.js
+++ static/js/editor/ckeditor/samples/sample.js
@@ -1,5 +1,5 @@
-/**
- * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+﻿/**
+ * Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -48,4 +48,3 @@
 		}
 	} );
 } )();
-// %LEAVE_UNMINIFIED% %REMOVE_LINE%
