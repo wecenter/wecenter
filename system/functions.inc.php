170201b7556750fe23a2a5cb770d02cfabaa6162
hponiang <hponiang@qq.com>
2015-07-04 21:32:23 +0800

    Update functions.inc.php

--- system/functions.inc.php
+++ system/functions.inc.php
@@ -794,7 +794,7 @@ function get_login_cookie_hash($user_name, $password, $salt, $uid, $hash_passwor
 		$password = compile_password($password, $salt);
 	}
 
-	$auth_hash_key = md5(G_COOKIE_HASH_KEY . $_SERVER['HTTP_USER_AGENT'] . $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+	$auth_hash_key = md5(G_COOKIE_HASH_KEY . $_SERVER['HTTP_USER_AGENT'] . strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']));
 
 	return H::encode_hash(array(
 		'uid' => $uid,
