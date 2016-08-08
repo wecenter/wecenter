f6ee97adc20201f74c80446e1ea77f0158e64ef3
Jat <jat@sinosky.org>
2016-08-08 12:32:43 +0800

    已修改        system/core/crypt.php

--- system/core/crypt.php
+++ system/core/crypt.php
@@ -28,7 +28,7 @@ class core_crypt
 
         mcrypt_generic_init($mcrypt, $this->get_key($mcrypt, $key), mcrypt_create_iv(mcrypt_enc_get_iv_size($mcrypt), MCRYPT_RAND));
 
-        $result = mcrypt_generic($mcrypt, gzcompress($data));
+        $result = mcrypt_generic($mcrypt, base64_encode(gzcompress($data)));
 
         mcrypt_generic_deinit($mcrypt);
         mcrypt_module_close($mcrypt);
@@ -38,12 +38,8 @@ class core_crypt
 
     public function decode($data, $key = null)
     {
-        if (strstr($data, '|'))
+        if ($algorithm = strstr($data, '|', true))
         {
-			$decode_data = explode('|', $data);
-
-			$algorithm = $decode_data[0];
-
             $data = str_replace($algorithm . '|', '', $data);
 
             $data = $this->hex_to_str($data);
@@ -63,6 +59,11 @@ class core_crypt
 
         mcrypt_generic_deinit($mcrypt);
         mcrypt_module_close($mcrypt);
+
+        if ($_result = base64_decode($result))
+        {
+            return gzuncompress($_result);
+        }
 
         return gzuncompress($result);
     }
